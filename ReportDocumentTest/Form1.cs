bffd6ae57f1cb30eee580946f42425d02af6d559
Juan Pleitez <juan.pleitez@ncubo.com>
2022-06-06 14:35:31 -0600

    Add project files.

---
+++ ReportDocumentTest/Form1.cs
@@ -0,0 +1,21 @@
+﻿using CrystalDecisions.CrystalReports.Engine;
+using System;
+using System.Windows.Forms;
+
+namespace ReportDocumentTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ReportDocument reportDocument = new ReportDocument();
+            crystalReportViewer1.ReportSource = covidReport1;
+
+        }
+    }
+}
